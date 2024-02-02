b5c629e63cba9ed99206680c1ea63aeb6918b36d
beans9926 <94064166+beans9926@users.noreply.github.com>
2024-02-02 09:01:33 +0530

    added struct required for calculation

--- src/avgVel.cpp
+++ src/avgVel.cpp
@@ -1,1 +1,11 @@
 #include <iostream>
+#include <vector>
+
+struct {
+  unsigned int timeSecStart;
+  unsigned int timeSecEnd;
+  unsigned int accuracyRect;
+  unsigned int highPowerEqu;
+  std::vector<int> *coefficients = new std::vector<int>;
+  std::vector<long double> *integFuncSummation = new std::vector<long double>;
+} velEqu;
